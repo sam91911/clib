81fc0e800d55dd8c565b6ee09a4f7c15fdac9510
sam91911 <nzsj302@gmail.com>
2023-08-10 17:13:37 +0800

    haha

---
+++ weblib/http.h
@@ -0,0 +1,14 @@
+#ifndef HTTP_H
+#define HTTP_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include "../typelib/dict.h"
+typedef struct{
+	int test;
+} HTTP_t;
+
+#endif
